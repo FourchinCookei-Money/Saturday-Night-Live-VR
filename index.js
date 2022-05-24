f37633cbf2b6ab287cff658b0cb5fc40f127e602
Fourchin Cookei Money <94145203+FourchinCookei-Money@users.noreply.github.com>
2022-05-23 21:27:26 -0700

    Ocean Stream Night Life VR WebGL

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
-var http = require("http");
+import { optimize } from "webpack";
 
-//create a server object:
-http
-  .createServer(function(req, res) {
-    res.write("Hello World!"); //write a response to the client
-    res.end(); //end the response
-  })
-  .listen(8080); //the server object listens on port 8080
+var PLUGINS = [];
+if (process.env.NODE_ENV === "production") {
+  PLUGINS.push(new optimize.UglifyJsPlugin());
+}
+
+export const entry = "./src/index.js";
+export const output = {
+  path: __dirname,
+  filename: "build/build.js"
+};
+export const plugins = PLUGINS;
+export const devServer = {
+  disableHostCheck: true
+};
